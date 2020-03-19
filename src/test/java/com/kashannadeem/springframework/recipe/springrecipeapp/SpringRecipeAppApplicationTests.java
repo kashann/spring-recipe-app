a2a7aa90c0fe5b556421566b1b412810ef5d6ed6
Kashan <kashi_nadim@yahoo.com>
2020-03-19 23:55:45 +0200

    Initial commit

---
+++ src/test/java/com/kashannadeem/springframework/recipe/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashannadeem.springframework.recipe.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
