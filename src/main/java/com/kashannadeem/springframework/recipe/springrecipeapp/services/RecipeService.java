0c9b71f6d2453d12755479f57ef8d5f5ade3485e
Kashan <kashi_nadim@yahoo.com>
2020-03-28 20:08:49 +0200

    Added RecipeBootstrap and displayed on index.html

---
+++ src/main/java/com/kashannadeem/springframework/recipe/springrecipeapp/services/RecipeService.java
@@ -0,0 +1,10 @@
+package com.kashannadeem.springframework.recipe.springrecipeapp.services;
+
+import com.kashannadeem.springframework.recipe.springrecipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
