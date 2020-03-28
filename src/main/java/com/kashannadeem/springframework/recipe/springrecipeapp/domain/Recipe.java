64c1e451377e714071b024c2d783b704c533859e
Kashan <kashi_nadim@yahoo.com>
2020-03-28 21:12:48 +0200

    Refactored code for bidirectional relationships

--- src/main/java/com/kashannadeem/springframework/recipe/springrecipeapp/domain/Recipe.java
+++ src/main/java/com/kashannadeem/springframework/recipe/springrecipeapp/domain/Recipe.java
@@ -117,6 +117,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Difficulty getDifficulty() {
@@ -129,6 +130,12 @@ public class Recipe {
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public void setIngredients(Set<Ingredient> ingredients) {
