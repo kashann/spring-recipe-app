e52fb7b54dd59e2b71b43da829da5e39da123b5e
Kashan <kashi_nadim@yahoo.com>
2020-04-05 23:07:41 +0300

    Added Integration Testing for UnitOfMeasureRepository

---
+++ src/test/java/com/kashannadeem/springframework/recipe/springrecipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,38 @@
+package com.kashannadeem.springframework.recipe.springrecipeapp.repositories;
+
+import com.kashannadeem.springframework.recipe.springrecipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescriptionTeaspoon() {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
