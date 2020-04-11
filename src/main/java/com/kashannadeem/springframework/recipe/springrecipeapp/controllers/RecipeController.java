4339b236da6acf0ddc214f470847603b2702bf32
Kashan <kashi_nadim@yahoo.com>
2020-04-11 22:02:35 +0300

    Added New Recipe Form and controller methods + testing

--- src/main/java/com/kashannadeem/springframework/recipe/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/com/kashannadeem/springframework/recipe/springrecipeapp/controllers/RecipeController.java
@@ -1,11 +1,12 @@
 package com.kashannadeem.springframework.recipe.springrecipeapp.controllers;
 
+import com.kashannadeem.springframework.recipe.springrecipeapp.commands.RecipeCommand;
+import com.kashannadeem.springframework.recipe.springrecipeapp.domain.Recipe;
 import com.kashannadeem.springframework.recipe.springrecipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -23,5 +24,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
--- src/test/java/com/kashannadeem/springframework/recipe/springrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/kashannadeem/springframework/recipe/springrecipeapp/controllers/RecipeControllerTest.java
@@ -1,5 +1,7 @@
 package com.kashannadeem.springframework.recipe.springrecipeapp.controllers;
 
+import com.kashannadeem.springframework.recipe.springrecipeapp.commands.RecipeCommand;
+import com.kashannadeem.springframework.recipe.springrecipeapp.converters.*;
 import com.kashannadeem.springframework.recipe.springrecipeapp.domain.Recipe;
 import com.kashannadeem.springframework.recipe.springrecipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,9 +11,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class RecipeControllerTest {
@@ -40,5 +44,32 @@ class RecipeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    void newRecipe() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/recipe/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeForm"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    void saveOrUpdate() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+        RecipeToRecipeCommand converter = new RecipeToRecipeCommand(new CategoryToCategoryCommand(),
+                new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand()),
+                new NotesToNotesCommand());
+        RecipeCommand command = converter.convert(recipe);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/show/" + command.getId()));
     }
 }
